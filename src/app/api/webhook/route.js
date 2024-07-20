9a9b5ff0a5fe3725f4d5830de9ba3eff7b9ad68c
tishyakhanna97 <tishya.ippo@gmail.com>
2024-07-20 21:14:54 +0800

    add formsg decryption

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,8 +1,33 @@
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import formsg from "@opengovsg/formsg-sdk";
+
+const formsgInstance = formsg();
+const POST_URI = "https://issue-tracker-gules-delta.vercel.app/api/webhook"
+const formSecretKey = process.env.FORMSG_API_KEY
+
+
 export async function POST(request) {
-    const data = await request.json();
+    try {
+        const signature = headers().get("X-Formsg-Signature");
+        formsgInstance.webhooks.authenticate(signature, POST_URI)
+    } catch (e) {
+        return NextResponse.json({ error: "Invalid signature" }, { status: 401 });
+    }
 
-    // Process the form data here
-    console.log('Form response received:', data);
+    try {
+        const data = await request.json();
+        const { attachmentDownloadUrls } = data;
+        const hasAttachment = attachmentDownloadUrls && attachmentDownloadUrls.length > 0;
+        const submission = hasAttachment
+            ? await formsgInstance.crypto.decryptWithAttachments(formSecretKey, body.data)
+            : await formsgInstance.crypto.decrypt(formSecretKey, body.data)
 
-    // Respond to the webhook sender
+        console.log(submission)
+
+        return NextResponse.json({ message: "Received Response" }, { status: 200 });
+
+    } catch (e) {
+        return NextResponse.json({ error: "Unable to decrypt" }, { status: 401 });
+    }
 }
